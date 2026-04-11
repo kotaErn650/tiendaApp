4cfc58735ce6b2bb7f0db0027eb65120434c0a5c
Jose Hernando Lopez <kota012011@gmail.com>
2026-04-10 22:10:01 -0500

    Delete ShopApp directory

--- ShopApp/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace ShopApp.Models;
-
-public record Category
-{
-    public int Id { get; set; }
-    public string Nombre { get; set; } = string.Empty;
-
-    public ICollection<Product> Products { get; set; } = new List<Product>();
-}
